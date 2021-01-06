6035c2b13503e5ea96df6a77449d9c94027f61dd
Emanuele Barban <58031+McRipper@users.noreply.github.com>
2021-01-06 16:58:32 +0100

    WIP

--- lib/trestle/filters/controller.rb
+++ lib/trestle/filters/controller.rb
@@ -2,10 +2,6 @@ module Trestle
   module Filters
     module Controller
       def index
-        if admin.filtrable? && params[:q].present?
-          breadcrumb t("admin.search.results", default: "Search Results"), { q: params[:q] }
-        end
-
         super
       end
     end
--- lib/trestle/filters/resource.rb
+++ lib/trestle/filters/resource.rb
@@ -3,12 +3,42 @@ module Trestle
     module Resource
       extend ActiveSupport::Concern
 
+      included do
+        # Include custom #collection method on Resource instance
+        prepend Collection
+
+        # Include custom #collection method on Resource class
+        singleton_class.send(:prepend, Collection)
+      end
+
+      module Collection
+
+        def collection(params={})
+          if filtrable?
+            model.ransack(params[:q]).result
+          else
+            super
+          end
+        end
+
+        #def finalize_collection(collection)
+          #Rails.logger.debug("finalize #{collection.inspect}")
+          #collection.result
+        #end
+
+        #def paginate(collection, params)
+          #collection.result.page(params[:page])
+        #end
+
+        def filters(&block)
+          Trestle::Form.new(&block)
+        end
+
+      end
+
       module ClassMethods
-
-        attr_accessor :filters
-
         def filtrable?
-          adapter.respond_to?(:ransack)
+          adapter.respond_to?(:filters)
         end
       end
     end
